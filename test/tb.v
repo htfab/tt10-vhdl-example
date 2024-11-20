f964a749ef1b0b576c3ba76f35f782db8ea66215
htfab <mpw@htamas.net>
2024-11-20 01:36:29 +0100

    feat: add sources from tt09-vhdl-example

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,9 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  wire [6:0] segments = uo_out[6:0];
+
+  tt_um_vhdl_seven_segment_seconds user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
